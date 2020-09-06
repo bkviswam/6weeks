08524a29de639b13f799c99c2500885c73a638df
Baijumon Viswambharan <baiju.k.viswam@gmail.com>
2020-09-05 17:12:03 -0700

    MyQueue Q Impl

---
+++ m1/MyQueue.java
@@ -0,0 +1,71 @@
+public class MyQueue {
+
+	public  class Node {
+		int data;
+		Node next; 
+		public Node(int data){
+			this.data = data;
+		}
+	}
+
+	public Node head;
+	public Node tail;
+
+	public boolean isEmpty(){
+		return head == null;
+	}
+
+	public int peek(){
+		if (!isEmpty()){
+			return head.data;
+		}
+		return -1;
+	}
+	public void add(int data){
+		Node node = new Node(data);
+		if(tail !=null){
+			tail.next = node;
+		}
+		tail = node;
+		if (head==null){
+			head = node;
+		}
+	}
+
+	public int remove(){
+		int data = head.data;
+		head = head.next;
+		if(head == null){
+			tail = null;
+		}
+		return data;
+	}
+
+	public static void main(String[] args) {
+		MyQueue q = new MyQueue();
+
+		System.out.println(q.isEmpty()); 
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(100);
+		q.add(200);
+		q.add(300);
+		System.out.println("Peek --: "+q.peek());
+		q.display(q.head);
+		System.out.println("Removed --: "+q.remove());
+		System.out.println("Removed --: "+q.remove());
+		q.display(q.head);
+		System.out.println("Peek --: "+q.peek());
+
+	}
+
+	public void display(Node node){
+		while(node!=null){
+			System.out.print(node.data+"-->");
+			node = node.next;
+		}
+		System.out.println("null");
+	}
+
+}
