23f29e885d7f3a20cddb7040d727c8820be8f10f
Baijumon Viswambharan <baiju.k.viswam@gmail.com>
2020-09-27 14:34:37 -0700

    Modified MFIA

--- m2/MostFrequentItemInArray.java
+++ m2/MostFrequentItemInArray.java
@@ -9,26 +9,24 @@ Find Most frequent item in an array
 public class MostFrequentItemInArray{
 	public int findMostFrequent(int[] nums){
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		int max_count = 0 ; 
+		int max_count = -1 ; 
 		int max_Item = 0 ;
 		for(int i : nums){
 			if(!map.containsKey(i)){
 				map.put(i, 1);
 			}else {
-				int max = map.get(i)+1;
-				map.put(i, max);
-				if(max >max_count){
-					max_count = max;
-					max_Item = i;
-				}
-
+				map.put(i,map.get(i)+1);
+			}
+			if(map.get(i) > max_count){
+				max_count = map.get(i);
+				max_Item = i;
 			}
 		}
 		return max_Item;
 	}
 	public static void main(String[] args) {
 		MostFrequentItemInArray mf = new MostFrequentItemInArray();
-		int [] nums = new int[] {1, 5, 5, 3, 5, 5, 1,2,1,1,1,3,1,3,2,3,5};
+		int [] nums = new int[] {5, 3, 5, 5, 2,1,1,1,3,2,3,5};
 		System.out.println(mf.findMostFrequent(nums));
 	}
 }
