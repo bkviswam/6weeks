726f5a38d1262241be507ade625fcd8b852e2da4
Baijumon Viswambharan <baiju.k.viswam@gmail.com>
2020-10-12 03:59:01 -0700

    RotateArrayByKElements

--- m1/RotateArrayByKElements.java
+++ m1/RotateArrayByKElements.java
@@ -3,10 +3,9 @@ import java.util.Arrays;
 public class RotateArrayByKElements{
 	private int[] rotateArrayRight(int [] nums, int k){
 		int n = nums.length;
-		int temp = 0;
 		while(k > 0){
 			for(int i=0; i<n-1; i++){
-				temp = nums[i];
+				int temp = nums[i];
 				nums[i] = nums[i+1];
 				nums[i+1] = temp;
 			}
@@ -17,10 +16,9 @@ public class RotateArrayByKElements{
 
 	private int[] rotateArrayLeft(int [] nums, int k){
 		int n = nums.length;
-		int temp = 0;
 		while(k > 0){
 			for(int i=n-1; i>0; i--){
-				temp = nums[i];
+				int temp = nums[i];
 				nums[i] = nums[i-1];
 				nums[i-1] = temp;
 			}
@@ -32,7 +30,7 @@ public class RotateArrayByKElements{
 	public static void main(String[] args) {
 		RotateArrayByKElements ra = new RotateArrayByKElements();
 		int nums[] = new int[]{1,2,3,4,5,6};
-		int k =4;
+		int k = 5;
 		System.out.println("Original Array "+Arrays.toString(nums));
 		System.out.println("Rotated right "+k+" times Array "+Arrays.toString(ra.rotateArrayRight(nums, k)));
 		System.out.println("Rotated left "+k+" times Array "+Arrays.toString(ra.rotateArrayLeft(nums, k)));
