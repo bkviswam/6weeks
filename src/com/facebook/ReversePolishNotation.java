03097c3045a4bcc712084c39d6066a19dafbf304
Baijumon Viswambharan <baiju.k.viswam@gmail.com>
2021-03-05 17:48:57 -0800

    Reverse Polish Notation

---
+++ src/com/facebook/ReversePolishNotation.java
@@ -0,0 +1,80 @@
+package com.facebook;
+
+import java.util.Stack;
+
+/**
+ * 150. Evaluate Reverse Polish Notation
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * <p>
+ * Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+ * <p>
+ * Note that division between two integers should truncate toward zero.
+ * <p>
+ * It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: tokens = ["2","1","+","3","*"]
+ * Output: 9
+ * Explanation: ((2 + 1) * 3) = 9
+ * Example 2:
+ * <p>
+ * Input: tokens = ["4","13","5","/","+"]
+ * Output: 6
+ * Explanation: (4 + (13 / 5)) = 6
+ * Example 3:
+ * <p>
+ * Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+ * Output: 22
+ * Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+ * = ((10 * (6 / (12 * -11))) + 17) + 5
+ * = ((10 * (6 / -132)) + 17) + 5
+ * = ((10 * 0) + 17) + 5
+ * = (0 + 17) + 5
+ * = 17 + 5
+ * = 22
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= tokens.length <= 104
+ * tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+ */
+public class ReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (!"+-*/".contains(token)) {
+                stack.push(Integer.valueOf(token));
+                continue;
+            }
+            int number2 = stack.pop();
+            int number1 = stack.pop();
+            int result = 0;
+            switch (token) {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+            }
+            stack.push(result);
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        ReversePolishNotation rpn = new ReversePolishNotation();
+        String[] tokens = new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+        System.out.println("Result of Reverse Polish Notation " + rpn.evalRPN(tokens));
+    }
+}
