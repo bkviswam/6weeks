9c7cae72aa7eb684cbee2d4c62d1edd6d1633e06
Baijumon Viswambharan <baiju.k.viswam@gmail.com>
2021-03-05 16:30:32 -0800

    Average Level of Binary Tree - BFS

---
+++ src/com/facebook/AvgOfLevelsBtreeBFS.java
@@ -0,0 +1,57 @@
+package com.facebook;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ *
+ * 637. Average of Levels in Binary Tree  // Easy
+ *
+ * Share
+ * Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+ * Example 1:
+ * Input:
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * Output: [3, 14.5, 11]
+ * Explanation:
+ * The average value of nodes on level 0 is 3,  on level 1 is 14.5, and on level 2 is 11. Hence return [3, 14.5, 11].
+ * Note:
+ * The range of node's value is in the range of 32-bit signed integer.
+ */
+
+
+public class AvgOfLevelsBtreeBFS {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if(root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            double sum = 0.0;
+            for(int i=0; i<size; i++){
+                TreeNode node = queue.remove();
+                sum += node.val;
+                if(node.left!=null) queue.add(node.left);
+                if(node.right!=null) queue.add(node.right);
+            }
+            result.add(sum/size);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        AvgOfLevelsBtreeBFS t = new AvgOfLevelsBtreeBFS();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println("Average Of levels of BTree "+t.averageOfLevels(root));
+    }
+}
