886b8ab3790ef1d850f6d9ce8837954b96880076
Baijumon Viswambharan <baiju.k.viswam@gmail.com>
2021-03-05 17:51:11 -0800

    Reverse Polish Notation

--- m2/ReverseWordsII.java
+++ m2/ReverseWordsII.java
@@ -2,50 +2,49 @@ import java.util.Arrays;
 
 
 /**
-
-186. Reverse Words in a String II // Medium
-
-Given an input string , reverse the string word by word. 
-
-Example:
-
-Input:  ['t','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e']
-Output: ['b','l','u','e',' ','i','s',' ','s','k','y',' ','t','h','e']
-Note: 
-
-A word is defined as a sequence of non-space characters.
-The input string does not contain leading or trailing spaces.
-The words are always separated by a single space.
-Follow up: Could you do it in-place without allocating extra space?
-
-**/
+ * 186. Reverse Words in a String II // Medium
+ * <p>
+ * Given an input string , reverse the string word by word.
+ * <p>
+ * Example:
+ * <p>
+ * Input:  ['t','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e']
+ * Output: ['b','l','u','e',' ','i','s',' ','s','k','y',' ','t','h','e']
+ * Note:
+ * <p>
+ * A word is defined as a sequence of non-space characters.
+ * The input string does not contain leading or trailing spaces.
+ * The words are always separated by a single space.
+ * Follow up: Could you do it in-place without allocating extra space?
+ **/
 
 
 public class ReverseWordsII {
-	public static void main(String[] args) {
-		ReverseWordsII rII = new ReverseWordsII();
-		char[] s = new char[] {'t','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e'};
-		rII.reverseWords(s);
-		System.out.println(Arrays.toString(s));
-	}
-	public void reverseWords(char[] s){
-		reverse(s, 0, s.length-1);
-		int prev = 0;
-		for(int i = 0; i <= s.length; i++){
-			if(i==s.length || s[i]==' '){
-				reverse(s, prev, i-1);
-				prev = i+1;
-			}
-		}
-	}
+    public static void main(String[] args) {
+        ReverseWordsII rII = new ReverseWordsII();
+        char[] s = new char[]{'t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'};
+        rII.reverseWords(s);
+        System.out.println(Arrays.toString(s));
+    }
 
-	private void reverse(char [] s, int start, int end){
-		while(start < end){
-			char t = s[start];
-			s[start] = s[end];
-			s[end] = t;
-			start++;
-			end--;
-		}
-	}
+    public void reverseWords(char[] s) {
+        reverse(s, 0, s.length - 1);
+        int prev = 0;
+        for (int i = 0; i <= s.length; i++) {
+            if (i == s.length || s[i] == ' ') {
+                reverse(s, prev, i - 1);
+                prev = i + 1;
+            }
+        }
+    }
+
+    private void reverse(char[] s, int start, int end) {
+        while (start < end) {
+            char t = s[start];
+            s[start] = s[end];
+            s[end] = t;
+            start++;
+            end--;
+        }
+    }
 }
